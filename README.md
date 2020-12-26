246584d6517a397a236d44d4ede394c55480d427
J-Rozas <jrozaslima@gmail.com>
2020-12-26 23:48:45 +0000

    First commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+Project that is part of TOP (Ruby). 
+Link to the project: https://www.theodinproject.com/courses/ruby-programming/lessons/sub-strings
---
+++ sub_strings.rb
@@ -0,0 +1,15 @@
+def substrings(string, array)
+    final = Hash.new(0)
+    string.downcase.split(" ").each do |element|
+        array.each do |thing|
+           if element.include?(thing)
+                final[thing] +=1
+           end
+        end
+    end
+    final
+end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
